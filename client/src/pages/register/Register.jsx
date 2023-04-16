8a8d3d469c76932c4b048e399e4de816935b5e41
felixNyalenda <felixasejunior@gmail.com>
2023-04-17 00:18:17 +0300

    reg page build

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import "./Register.css";
 import { useState } from "react";
@@ -7,6 +7,9 @@ const Register = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,9 +23,11 @@ const Register = () => {
     const response = await fetch("http://localhost:8800/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      // body: JSON.stringify( newUser ),
-      body: JSON.stringify({ username, password, email }),
+      body: JSON.stringify(newUser),
+      // body: JSON.stringify({ username, password, email }),
     });
+
+    response.status === 200 ? navigate("/") : setError(true);
   };
 
   return (
@@ -70,6 +75,12 @@ const Register = () => {
               Register
             </button>
           </form>
+
+          {error && (
+            <span className={"reg-error"} style={error && { display: "flex" }}>
+              Registration failed❗ Try again later.
+            </span>
+          )}
         </div>
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,13 +1,35 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import bcrypt from "bcrypt";
+
+import User from "./models/User.js";
+
+dotenv.config();
+
+const salt = bcrypt.genSaltSync(10);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/register", (req, res) => {
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGO_URL);
+
+app.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
-  res.json({ requestData: { username, email, password } });
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+      email,
+    });
+    res.json(userDoc);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(error);
+  }
 });
 
 app.listen(8800);
